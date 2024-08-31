62366a5e71a8da0a5ebd749cce981fc79c34fa46
Muhammed Elfawy <105250350+muhammedelfawy@users.noreply.github.com>
2024-08-31 20:16:15 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TestmonialsComponent } from './components/testmonials/testmonials.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "admin", component: AdminComponent },
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactusComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "testimonials", component: TestmonialsComponent },
 ];
 
 @NgModule({
